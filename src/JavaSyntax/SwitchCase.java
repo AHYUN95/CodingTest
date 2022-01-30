fedacdd11c382105f329e586af4309c256292569
unknown <ahyunsss@naver.com>
2022-01-30 16:10:15 +0900

    0130

---
+++ src/JavaSyntax/SwitchCase.java
@@ -0,0 +1,32 @@
+package JavaSyntax;
+
+import java.util.Scanner;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+	
+		/*
+		
+		switch(ch69) {
+		case 'A' :
+		System.out.println("best!!!");
+			break;
+		case 'B' :
+		System.out.println("good!!!");
+			break;
+		case 'C' :
+		System.out.println("run!!!");
+			break;
+		case 'D' :
+		System.out.println("slowly!!!");
+			break;
+		default :
+			System.out.println("what?");	
+			break;
+		}
+		 */
+
+	}
+
+}
