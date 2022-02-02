99ea957bf60aa9a658ef5d2d2f5b3896ce27dbb7
unknown <ahyunsss@naver.com>
2022-02-02 23:58:46 +0900

    0202 Hanoi update

--- 2.Baekjoon_ProblemSet/recursion/Hanoi_11729.java
+++ 2.Baekjoon_ProblemSet/recursion/Hanoi_11729.java
@@ -6,22 +6,28 @@ import java.io.InputStreamReader;
 
 public class Hanoi_11729 {
 
-	public static void main(String[] args)throws IOException {
+	static StringBuilder sb;
 	
+	public static void main(String[] args)throws IOException {
+		sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		System.out.println(hanoiNum(n));
 		hanoiM(n,1,2,3);
+		System.out.println(hanoiNum(n));
+		System.out.println(sb.toString());
 
 	}
+	
 	//n = 하노이 몇층, from= 시작 칸, to = 도착지, by = 나머지
 	static void hanoiM(int n, int from, int by, int to) {
 		
 		if (n==1) {
-			System.out.printf("%d %d%n", from,to);
+			sb.append(from+" "+to+"\n");
+			//System.out.printf("%d %d%n", from,to);
 		}else {
 			hanoiM(n-1,from,to,by);
-			System.out.printf("%d %d%n", from,to);
+			//System.out.printf("%d %d%n", from,to);
+			sb.append(from+" "+to+"\n");
 			hanoiM(n-1,by,from,to);
 		}
 	}
