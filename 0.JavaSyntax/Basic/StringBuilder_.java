48dd4bc9ea098c3852cbfa7432791606f86449f7
unknown <ahyunsss@naver.com>
2022-02-03 00:57:25 +0900

    0202

--- 0.JavaSyntax/Basic/StringBuilder_.java
+++ 0.JavaSyntax/Basic/StringBuilder_.java
@@ -9,7 +9,11 @@ public class StringBuilder_ {
 		
 	StringBuilder sb = new StringBuilder(); // StringBuilder 생성자
 	for(int i=0;i<=10; i++) {
-	sb.append("Hello2\n");
+	int k =3;
+	sb.append(k+"\n");
+	int [] arr =new int [1];
+	arr[0] =2;
+	sb.append(arr[0]+"\n");
 	}	
 	
 	System.out.println(sb);
@@ -17,6 +21,7 @@ public class StringBuilder_ {
 	* 모아서 한번에 출력, 반복해서 출력해야 하는 경우는 StringBuilder사용
 	*sb.toString() 하지 않아도 출력 결과가 나오는 이유는 StringBuilder자체에
 	*toString이 오버라이딩 되어있기 때문
+	*++오바라이딩 되어서 int, arr, char도 사용 가능
 	*/
 		
 	}
