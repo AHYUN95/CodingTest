abb585dc3f9dde03dfabe8fa75e2f32c08cfdf95
unknown <ahyunsss@naver.com>
2022-02-04 22:30:01 +0900

    TernaryOperator

--- 0.JavaSyntax/Conditional/TernaryOperator.java
+++ 0.JavaSyntax/Conditional/TernaryOperator.java
@@ -4,18 +4,20 @@ public class TernaryOperator {
 
 	public static void main(String[] args) {
 		// if-else표현
-		int a;
-		if (5<4) {
-			a=50;
+		int c;
+		int a = 5;
+		int b = 4;
+		if (a<b) {
+			c=50;
 		}else {
-			a=40;}
+			c=40;}
 		
-		System.out.println(a); // 결과 = 40
+		System.out.println(c); // 결과 = 40
 		
 		//삼항연산자 표현
 		// 조건식?(참일때의 값): (거짓일떄의 값)ㄴ
-		int b = (5<4)? 50 :40;
-		System.out.println(b); // 결과 = 40
+		int d = (a<b)? 50 :40;
+		System.out.println(d); // 결과 = 40
 		
 		//3개 비교
 		//(a>b ? a:b)>c ? (a>b ? a:b):c
