5d5a5bdcf84ccb3417319dc315f8a916842e107b
unknown <ahyunsss@naver.com>
2022-02-04 19:31:33 +0900

    Director

---
+++ 2.Baekjoon_ProblemSet/BruteForce/director.java
@@ -0,0 +1,27 @@
+package BruteForce;
+import java.util.Scanner;
+public class director {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int num =665;
+		int count = 0;
+		
+		while(true) {
+			num++;
+			String str = String.valueOf(num);
+			if(str.contains("666")) count++;
+			
+			if(count == n) {
+				break;
+			}
+			
+		}
+		System.out.println(num);
+		sc.close();
+	}
+
+}
