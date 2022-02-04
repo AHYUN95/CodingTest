46c16ae648c0941718fd963ddc9ac1c07098189a
unknown <ahyunsss@naver.com>
2022-02-04 22:33:50 +0900

    Scanner

--- 0.JavaSyntax/InputOutput/Scanner_Class.java
+++ 0.JavaSyntax/InputOutput/Scanner_Class.java
@@ -13,6 +13,7 @@ public class Scanner_Class {
 	String h = test.next(); 		// String 형 입력 및 리턴 (공백을 기준으로 한 단어를 읽음)
 	String i = test.nextLine(); 	// String 형 입력 및 리턴 (개행을 기준으로 한 줄을 읽음)
 	
+	System.out.println(c+f+h+i);
 	test.close(); // Scanner 종료하기
 	
 	
@@ -20,12 +21,15 @@ public class Scanner_Class {
 	int num = test.nextInt();
 	String str = test.nextLine(); // 입력이 안됨 오류
 	
+	System.out.println(num+str);
+	
 	//nextInet의 경우 엔터키 누르기 전의 입력만 받음 
-	//그래서 nextLine이 nextInt엔터의 입력을 받아서 메소드 종료된다.
+	//그래서 nextLine이 nextInt뒤에 남은 엔터의 입력을 받아서 메소드 종료된다.
 	int num2 = test.nextInt();
-	test.nextLine(); // 개행문자(/n)삭제
+	test.nextLine(); // 대신 값(엔터)값을 받아주면서 개행문자(/n)삭제
 	String str2 = test.nextLine();
 	
+	System.out.println(num2+str2);
 	
 	//nextInt,next의 경우 1 2 3 이렇게 입력 받아도 엔터 스페이스로 분리됨
 	Scanner s = new Scanner(System.in);
@@ -35,7 +39,7 @@ public class Scanner_Class {
 	System.out.println(num3);
 	System.out.println(num4);
 	System.out.println(num5);
-	
+	s.close();
 	}
 
 }
