e6b0a06863f0e0a6b2a6d147c0848b8ef5886863
unknown <ahyunsss@naver.com>
2022-02-07 23:06:37 +0900

    comparator 공부중!! 새벽공부 예정..

--- 0.JavaSyntax/Sort/Comparator_.java
+++ 0.JavaSyntax/Sort/Comparator_.java
@@ -1,9 +1,13 @@
 package Sort;
 
+/*
+ static void sort(Object[] a) //객체 배열에 저장된 Comparable에 의한 정렬
+ static void sort(Object[] a, Comparator c) // 지정한 Comparator(정렬기준)에 의한 정렬
+ */
 public class Comparator_ {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub//
 
 	}
 
