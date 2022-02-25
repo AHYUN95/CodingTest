1c5bf04ddf037313332c80ac65c19996b70e6c75
unknown <ahyunsss@naver.com>
2022-02-25 18:56:45 +0900

    DynamicProgramming

--- 2.Baekjoon_ProblemSet/DynamicProgramming/LCS_9251.java
+++ 2.Baekjoon_ProblemSet/DynamicProgramming/LCS_9251.java
@@ -4,56 +4,44 @@ import java.util.Scanner;
 
 public class LCS_9251 {
 
-	static String s1;
-	static String s2;
+	
 	static char c1 [];
 	static char c2 [];
-	
+	static Integer dp [][];
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		s1 = sc.next();
-		s2 = sc.next();
+		
+		c1 = sc.nextLine().toCharArray();
+		c2 = sc.nextLine().toCharArray();
+		
 		sc.close();
 		
-		c1 = new char [s1.length()];
-		c2 = new char [s2.length()];
+		dp = new Integer[c1.length][c2.length];
 		
-		for(int i=0; i<s1.length(); i++) {
-			c1 [i] = s1.charAt(i); 
-		}
 		
-		for(int i=0; i<s2.length(); i++) {
-			c2 [i] = s2.charAt(i); 
-		}
-		
-		System.out.println(LCS(0,0));
-		System.out.println(LCS(1,0));
-		System.out.println(LCS(2,0));
-		System.out.println(LCS(3,0));
-		System.out.println(LCS(4,0));
-		System.out.println(LCS(5,0));
-		/*
-		int Max = 0;
-		for(int i=0; i<s1.length();i++) {
-			Max = Math.max(Max,LCS(i,0));
-		}
-		
-		System.out.println(Max);
-	*/
+		System.out.println(LCS(c1.length-1, c2.length-1));
+
 	}
 	
 	//몇번째 c1에서 n번째 인덱스에 대해서 c2의 s번째 인덱스에서 부터 서치 
-	static int LCS(int n, int s) {
-			if(n==s1.length()) {
-				return 0;
+	static int LCS(int x, int y) {
+		
+		if(x==-1 || y==-1) {
+			return 0;
+		}
+
+		if(dp[x][y]==null) {
+			dp[x][y] = 0;
+			//간혹 값이 아닌 null을 참고하게 되어 잘못 연산 할 수 있어서 일단 0으로 초기화
+			//적지 않아도 잘 돌아감
+			if(c1[x]==c2[y]) {
+				dp[x][y] = LCS(x-1,y-1) + 1;
+				//LCS(x,y-1) +1 >> 이렇게 하면 같은 글자가 반복하는 경우 카운트가 2개 된다.
+				//A > CAPCAP // A가 같을 때 두번 더해짐
+			}else
+				dp[x][y] = Math.max(LCS(x-1,y), LCS(x,y-1));
 			}
 		
-			for(int i=s; i<s2.length();i++) {
-				if(c1[n]==c2[i]) {
-					return LCS(n+1,i+1) + 1;	
-					}
-				}
-				return 0;
+		return dp[x][y];
 		}			
 	}
