68b25df178370cacc3d7e5c4302094ee5b5d9cf1
unknown <ahyunsss@naver.com>
2022-02-28 23:25:12 +0900

    continue...

--- 2.Baekjoon_ProblemSet/DynamicProgramming/Knapsack_12865.java
+++ 2.Baekjoon_ProblemSet/DynamicProgramming/Knapsack_12865.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class Knapsack_12865 {
+public class Knapsack_12865{
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,23 +28,36 @@ public class Knapsack_12865 {
 				return c1[0]-c2[0];
 			}
 		});
+		
 		//dp에 앞에서 부터 더한 무게, 가치 넣어 줄꺼임
 		int dp [][] = new int [n][2];
-		dp[0][0] = wv[0][0];
-		dp[0][1] = wv[0][1];
 	
-
+		if(wv[0][0]<=k) {
+			
+			dp[0][0] = wv[0][0];
+			dp[0][1] = wv[0][1];
+			
 			for(int i=1; i<n; i++) {
 			if(dp[i-1][0]+wv[i][0]<=k) {
 				dp[i][1] = dp[i-1][1]+wv[i][1];
 				dp[i][0] = dp[i-1][0]+wv[i][0];	
 			}
 			else {
+				if(wv[i][0]<=k) {
+					dp[i][1] = Math.max(dp[i-1][1], wv[i][1]);
+					if(dp[i][0] == wv[i][0]) {
+						dp[i][1] = wv[i][1];
+					}else {
+						dp[i][1] =dp[i-1][1];
+					}
+				}else {oiiiiii
+				
 				dp[i][0] = dp[i-1][0];
 				dp[i][1] = dp[i-1][1];
+				}
 				for(int j=i-1; j>0; j--) {
 					if(dp[j][0]+wv[i][0]<=k) {
-						dp[i][1] = Math.max(dp[j][1]+wv[i][0],dp[i-1][1]);
+						dp[i][1] = Math.max(dp[j][1]+wv[i][1],dp[i-1][1]);
 						if(dp[i][1]==dp[i-1][1]) {
 							dp[i][0] = dp[i-1][0];
 						}else {
@@ -56,7 +69,9 @@ public class Knapsack_12865 {
 			}
 		}
 		System.out.println(dp[n-1][1]);
-
+		}else {
+			System.out.println(0);
+		}
 	}
 
 }
