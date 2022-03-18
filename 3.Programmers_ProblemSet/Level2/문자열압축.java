07906554bdbe98a1b0e6f274240349c0074bdbda
unknown <ahyunsss@naver.com>
2022-03-18 16:25:01 +0900

    72점이라는데 뭐가 틀렸는지;;

--- 3.Programmers_ProblemSet/Level2/문자열압축.java
+++ 3.Programmers_ProblemSet/Level2/문자열압축.java
@@ -1,54 +1,40 @@
 package Level2;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class 문자열압축 {
 
 	public static void main(String[] args) {
 		
-		solution("aabbaccc");
+		System.out.println(solution("acacacacabcdabcdaaeessced"));
 
 	}
 
 	static int solution (String s) {
-		int answer =0;
-		
-		for(int i=1; i<=s.length()/2; i++) {//자르는 단뒤
-			for(int j=0; j<s.length(); j+=i) {
-			String index = s.substring(0,i-1);
-			if(index == s.substring(j+i,j+i+i-1))
-		}
-	}
-		
-		return answer;
-}
-	static int count (int n) {
+		int answer =s.length();
 		
 		int count = 0;
-		
-		Map<String, Integer> hm = new HashMap<>();
-		
-		for(int i=0; i<len; i+=n){
-			String index = input.substring(i,i+n);
-			if(hm.containsKey(index)) {
-				hm.put(index, hm.get(index)+1);
-			}else {
-				hm.put(index, 1);
-			}
+
+		for(int i=1; i<=s.length()/2; i++) {//자르는 단뒤
+			
+			int check = 0; //앞에 꺼가 몇번 중복되는지 확인해줄 것임
+			String index = s.substring(0,i);
+			count = i + (s.length()%i);
+			
+			for(int j=i; j<=s.length()-i; j+=i) {
+				if(index.equals(s.substring(j,j+i)) && check == 0){ //문자열 비교는 ==(주소값비교) 말고 .equals로 해주자 제발~
+					count += 1;
+					check += 1;
+				}else if (index.equals(s.substring(j,j+i)) && check !=0) {
+					check += 1;
+				}else {
+					index = s.substring(j,j+i);
+					count+= i;  
+					check = 0;
+				}
 		}
-		
-		for(String key:hm.keySet()) {
-			if(Integer.parseInt(key)==1) {
-				count+=n;
-			}else {
-				count+=n+1;
-			}
-		}
-		
-		return count;
-		
-	}
-	
+			answer = Math.min(answer, count); //for문 돌면서 최소값을 추가해준다.
+	}	
+		return answer;
+}
+
 
 }
