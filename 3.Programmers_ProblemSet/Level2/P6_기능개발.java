6d409e07a05dbab163bc46d02447e989651ee09d
unknown <ahyunsss@naver.com>
2022-04-12 22:05:07 +0900

    90.9점..뭐가 문제야..

---
+++ 3.Programmers_ProblemSet/Level2/P6_기능개발.java
@@ -0,0 +1,45 @@
+package Level2;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class P6_기능개발 {
+
+	public static void main(String[] args) {
+		
+		int [] progresses = {1,2,3,6,7,3,98,65};
+		int [] speeds = {5,10,3,54,2,4,6,10};
+		
+		solution(progresses,speeds);
+
+	}
+
+	    static int[] solution(int[] progresses, int[] speeds) {
+	    	
+	    	Queue<Integer> queue = new LinkedList<Integer>();
+	        for(int i=0; i<progresses.length; i++){
+	            queue.offer((int)Math.ceil((100-progresses[i])/speeds[i]));
+	        }
+	        
+	        ArrayList<Integer> list = new ArrayList<Integer>();
+	        
+	        while(!queue.isEmpty()){
+	            int cnt = 1;
+	            int index = queue.poll();
+	            while(!queue.isEmpty() && queue.peek() <= index ){
+	                cnt++;
+	                queue.remove();
+	            }
+	            list.add(cnt);
+	        }
+	        
+	        int [] answer = new int [list.size()];
+	        for(int i=0; i<answer.length; i++){
+	            answer[i] = list.get(i);
+	            //test출력
+	            System.out.println(answer[i]);
+	        }
+	        return answer;
+	    }
+	}
